b6180ba0dcfd355c5d997ecfd6e57c9683199a08
Matias Berg <m.berg.10@aberdeen.ac.uk>
2013-11-02 18:31:40 +0000

    Initial commit

---
+++ src/com/example/munrotracker/Munro.java
@@ -0,0 +1,29 @@
+package com.example.munrotracker;
+public class Munro {
+	public String name;
+	public String region;
+	public String height;
+	public String gridref;
+	public String drop;
+	public String feature;
+	public String country;
+	public String streetmapurl;
+	public String geographurl;
+	public String lat;
+	public String lon;
+	public Munro(String input){
+		String[] bits = input.split(",");
+		int i = 0;
+		name = bits[i++];
+		region = bits[i++];
+		height = bits[i++];
+		gridref = bits[i++];
+		drop = bits[i++];
+		feature = bits[i++];
+		country = bits[i++];
+		streetmapurl = bits[i++];
+		geographurl = bits[i++];
+		lat = bits[i++];
+		lon = bits[i++];
+	}
+}
